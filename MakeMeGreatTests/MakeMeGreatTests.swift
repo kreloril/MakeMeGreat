047d2cfc9bc2531b0d57ac811cabcd179c72bc29
John Mulvey <jcmulvey@gmail.com>
2017-09-27 09:05:07 -0400

    Progress with model

--- MakeMeGreatTests/MakeMeGreatTests.swift
+++ MakeMeGreatTests/MakeMeGreatTests.swift
@@ -22,15 +22,46 @@ class MakeMeGreatTests: XCTestCase {
     }
     
     func testExample() {
+        
+        let mdl : AffrimModel = AffrimModel()
+      //  var carmdata1 : NSArray = ["affrim 1","Affrim 2","affrim 3"];
+        let dataset : NSDictionary = ["name":"dataname","data": ["affrim 1","Affrim 2","affrim 3","affrim 4","affrim 5","affrom 6"]]
+    
+        mdl.initData(dataset as! [AnyHashable : Any])
+        
+         mdl.setAffrimationValue(0)
+         //mdl.setAffrimationValue(2)
+         mdl.setAffrimationValue(3)
+        // mdl.setAffrimationValue(5)
+         mdl.setAffrimationValue(1)
+         mdl.setAffrimationValue(4)
+         mdl.printFoundTracking();
+        
+         for i in 0..<mdl.affrimations.count {
+        
+            print("Affrimation found \(mdl.isAssigned(i))")
+        }
+        
+        
+        mdl.affrimationTracking = 0;
+        
+        
+    
+        NSLog("name = %@, data = %@", mdl.catagoryName,mdl.affrimations);
+        
+       
+        
+        for _ in 0..<mdl.affrimations.count {
+             NSLog("rnd = %@", mdl.getAffrimation());
+        }
+        
+        mdl.printFoundTracking();
+        
+        
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+   
     
 }
